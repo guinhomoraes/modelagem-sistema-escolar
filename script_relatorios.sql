f9d0094f0a1e31359ea6b5757580f0226ece8d28
guinhomoraes <guilhermeguilherme98@gmail.com>
2024-12-22 10:14:04 -0300

    Update script_relatorios.sql

--- script_relatorios.sql
+++ script_relatorios.sql
@@ -1,3 +1,5 @@
+//Visão da diretora
+
 //Quantas escolas existem?
 
 select count(*) from escola where cidade = "Rio de Janeiro";
@@ -22,3 +24,30 @@ inner join escola e on e.id = p.id_escola
 group by e.nome_fantasia
 order by count(*) desc
 ;
+
+
+// Visão do professor
+
+//Quais são as minhas turmas atualmente?
+
+select ps.nome as nome_professor, p.registro, t.nome as nome_disciplina,
+t.status
+from turma as t
+inner join professor as p on p.id = t.id_professor
+inner join pessoa as ps on ps.id = p.id_pessoa
+where p.registro = 'PROF003'
+and t.status = 1
+order by ps.nome asc
+
+//Quais disciplinas que eu leciono?
+
+select distinct ps.nome as nome_professor, p.registro, d.nome as nome_disciplina
+from turma as t
+inner join professor as p on p.id = t.id_professor
+inner join pessoa as ps on ps.id = p.id_pessoa
+inner join turma_curso as ct on ct.id_turma = t.id
+inner join curso_disciplina as cd on cd.id_curso = ct.id_curso
+inner join disciplina as d on d.id = cd.id_disciplina
+where p.registro = 'PROF003'
+and t.status = 1
+order by d.nome asc;
